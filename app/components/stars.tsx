ab7ae3a9fc68be2c5655af9bbd1553d3bce859db
EVERYTHINGING <mikeparisi@gmail.com>
2024-04-10 22:58:17 -0400

    working post without body

--- app/components/stars.tsx
+++ app/components/stars.tsx
@@ -9,27 +9,27 @@ export default function Stars(props: any) {
 
   return (
     <div className="my-6 rating rating-lg">
-      <input 
-        type="radio" 
-        name={props.name} 
-        value="1" 
-        checked={rating === "1"} 
+      <input
+        type="radio"
+        name={props.name}
+        value="1"
+        checked={rating === "1"}
         onChange={onOptionChange}
         className="mask mask-star-2 bg-orange-400"
       />
-      <input 
-        type="radio" 
-        name={props.name} 
-        value="2" 
-        checked={rating === "2"} 
+      <input
+        type="radio"
+        name={props.name}
+        value="2"
+        checked={rating === "2"}
         onChange={onOptionChange}
         className="mask mask-star-2 bg-orange-400"
       />
-      <input 
-        type="radio" 
-        name={props.name} 
-        value="3" 
-        checked={rating === "3"} 
+      <input
+        type="radio"
+        name={props.name}
+        value="3"
+        checked={rating === "3"}
         onChange={onOptionChange}
         className="mask mask-star-2 bg-orange-400"
       />
--- app/components/survey.tsx
+++ app/components/survey.tsx
@@ -1,41 +1,54 @@
 'use client'
- 
+
 import { useSearchParams } from 'next/navigation'
 import { FormEvent, useState } from 'react'
 import Stars from './stars';
- 
+
 export default function Survey() {
-  const searchParams = useSearchParams()
- 
-  const firstName = searchParams.get('firstName');
-  const lastName = searchParams.get('lastName');
-  const agency = searchParams.get('agency');
-  const key = searchParams.get('key');
- 
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
- 
-    const formData = new FormData(event.currentTarget)
-    const response = await fetch('/api/submit', {
-      method: 'POST',
-      body: formData,
-    })
- 
-    // Handle response if necessary
-    const data = await response.json()
-    // ...
-  }
-  return (
-    <div className='text-center'>
-        <img className='inline-block' src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f917/512.gif" alt="😊" width="256" height="256" />
-        <h1 className="my-8 text-5xl font-bold">Hi, {firstName} {lastName} !</h1>
-        <h2 className="my-8 text-3xl">How would you rate your project with {agency} so far?</h2>
-        <form onSubmit={onSubmit}>
-            <Stars name={'rating'} />
-            <div>
-                <button className="btn btn-primary" type="submit">Submit</button>
-            </div>
-        </form>
-    </div>
-  );
+    const [submitted, setSubmitted] = useState(false);
+
+    const searchParams = useSearchParams()
+    const firstName = searchParams.get('firstName');
+    const lastName = searchParams.get('lastName');
+    const agencyName = searchParams.get('agencyName');
+    const surveyId = searchParams.get('surveyId');
+
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        setSubmitted(true);
+
+        const formData = new FormData(event.currentTarget);
+        const dataObj = JSON.stringify(Object.fromEntries(formData));
+        console.log(dataObj)
+
+        const response = await fetch('http://localhost:8055/flows/trigger/a4d3386d-8b9f-45cb-b623-01012c6ce83a', {
+            method: 'POST',
+            body: dataObj
+        }).catch(rejected => {
+            console.log(rejected);
+        });
+    }
+    return (
+        <div className='text-center'>
+            {!submitted ? (
+                <div>
+                    <img className='inline-block' src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f917/512.gif" alt="😊" width="256" height="256" />
+                    <h1 className="my-8 text-5xl font-bold">Hi, {firstName} {lastName} !</h1>
+                    <h2 className="my-8 text-3xl">How would you rate your project with {agencyName} so far?</h2>
+                    <form onSubmit={onSubmit}>
+                        <Stars name={'rating'} />
+                        <div>
+                            <button className="btn btn-primary" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            ) : (
+                <div>
+                    <h1 className="my-8 text-5xl font-bold">Thank you !</h1>
+                    <img className='inline-block' src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f973/512.gif" alt="😊" width="256" height="256" />
+                </div>
+            )}
+            
+        </div>
+    );
 }
